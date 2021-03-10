c3fc2a4016e1abdeb3443c774c344a75119c97f0
Mohammad Faraz <msf8tq@virginia.edu>
2021-03-10 12:50:41 -0500

    Adding Rev_LEC2.java to the repo

---
+++ Rev_LEC2.java
@@ -0,0 +1,13 @@
+public class Rev_LEC2{
+    // 'javac Rev_LEC2' = command to compile the file in the terminal
+    // java Rev_LEC2 = command to run the file in the terminal
+
+    public static void main(String[] args){
+        // 'void' = the method doesn't RETURN anything
+        // 'main' = the brain of our program
+        // 'String[] = the argument's data type'
+        System.out.println("Hello World");
+    }
+
+}
+
